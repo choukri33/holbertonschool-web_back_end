9d3a30da454645b9b7d47b002baa00eb1efced8d
choukri33 <7646@holbertonstudents.com>
2024-07-17 17:36:48 +0000

    test

--- ES6_basic/.eslintrc.js
+++ ES6_basic/.eslintrc.js
@@ -1,19 +1,36 @@
 module.exports = {
     env: {
-      browser: true,
+      browser: false,
       es6: true,
-      jest: true
+      jest: true,
     },
     extends: [
-      'eslint:recommended'
+      'airbnb-base',
+      'plugin:jest/all',
     ],
+    globals: {
+      Atomics: 'readonly',
+      SharedArrayBuffer: 'readonly',
+    },
     parserOptions: {
       ecmaVersion: 2018,
-      sourceType: 'module'
+      sourceType: 'module',
     },
+    plugins: ['jest'],
     rules: {
-      'no-var': 'error',
-      'prefer-const': 'error'
-    }
+      'no-console': 'off',
+      'no-shadow': 'off',
+      'no-restricted-syntax': [
+        'error',
+        'LabeledStatement',
+        'WithStatement',
+      ],
+    },
+    overrides:[
+      {
+        files: ['*.js'],
+        excludedFiles: 'babel.config.js',
+      }
+    ]
   };
   
